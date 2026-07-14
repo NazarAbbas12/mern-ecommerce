91af7a2e9e7b82001f9b91eb17d746c2ddb84af4
Nazar Abbas <nazarabbasmm4@gmail.com>
2026-07-14 23:18:24 +0500

    Started

---
+++ frontend/src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import Home from './pages/Home';
+import Collection from './pages/Collection';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Cart from './pages/Cart';
+import Login from './pages/Login';
+import Orders from './pages/Orders';
+import PlaceOrder from './pages/PlaceOrder';
+import Product from './pages/Product';
+
+const App = () => {
+  
+  <Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="/product/:productID" element={<Product />} />
+      </Routes>
+  </Routes>
+}
+
+export default App
