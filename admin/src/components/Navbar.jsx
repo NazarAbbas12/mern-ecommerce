5daf1d772ff1daa0b6ecb2177e32fa99b679418d
Nazar Abbas <nazarabbasmm4@gmail.com>
2026-09-04 12:47:19 +0500

    sidebar and navbar designed

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import {assests} from '../assets./assets'
-const App = () => {
+import React from "react";
+import { assets } from "../../../frontend/src/assets/forever-assets/assets/admin_assets/assets.js";
+const Navbar = () => {
   return (
-    <div className='flex items-centre py-2 px-[4%] justify-between'>
-     <img  className='w-[max(10%,80px)]' src={assests.logo} alt="" />
-     <button className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm' >Logout</button>
+    <div className="flex items-center justify-between py-2 px-[4%]">
+      <img className="w-[max(10%,80px)]" src={assets.logo} alt="logo" />
+      <button className="bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm">
+        Logout
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
