2a71554f137e93bbdec5eb9e61bd37dbd401212d
Ahmad-Bilal09 <ahmadbilalofficial29@gmail.com>
2026-09-05 02:41:06 +0500

    Resolve merge conflicts

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -4,6 +4,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  server:{port:5174}
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  server: {
+    port: 5174
+  }
 })
