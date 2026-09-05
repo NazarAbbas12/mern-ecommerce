4ed57b9b71cc2ff2ec91635db98316ea8a8be467
Ahmad-Bilal09 <ahmadbilalofficial29@gmail.com>
2026-09-05 05:15:51 +0500

    updated Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -12,7 +12,14 @@ const Navbar = () => {
 
     const [visible, setVisible] = useState(false)
 
-    const {setShowSearch, getCartCount} = useContext(ShopContext);
+    const {setShowSearch, getCartCount, navigate, token, setToken, setCartItems} = useContext(ShopContext);
+
+    const Logout = () => {
+        navigate('/login')
+        localStorage.removeItem('token')
+        setToken('')
+        setCartItems({})        
+    }
 
   return (
     <div className='flex items-center justify-between py-1 font-medium'>
@@ -45,18 +52,18 @@ const Navbar = () => {
 
                 <div className='group relative'>
 
-                    <Link to='/login'>                   
-                        <FiUser className='w-5 cursor-pointer'/>
-                    </Link>
-
-
-                    <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
+                                      
+                        <FiUser onClick={()=> token ? null : navigate('/login')} className='w-5 cursor-pointer'/>
+                    
+                        {/*Dropdown Menu*/}
+                        {token &&                     <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
                         <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
                             <p className='cursor-pointer hover:text-black'>My Profile</p>
-                            <p className='cursor-pointer hover:text-black'>Orders</p>
-                            <p className='cursor-pointer hover:text-black'>Logout</p>
+                            <p onClick={()=> navigate('/orders')} className='cursor-pointer hover:text-black'>Orders</p>
+                            <p onClick={logout} className='cursor-pointer hover:text-black'>Logout</p>
                         </div>
-                    </div>
+                    </div>}
+
                 </div>
                 <Link to='/cart' className='relative'>
                     <FiShoppingCart className="w-5 min-win-5" />
