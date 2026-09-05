95c25900416b0bde275d08d7ec1eff6db6300f39
Ahmad-Bilal09 <ahmadbilalofficial29@gmail.com>
2026-09-05 05:17:58 +0500

    Updated User Login, And Logout Function Completed

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -43,6 +43,12 @@ const Login = () => {
     }
   }
 
+  useEffect(()=>{
+    if(token){
+      navigate('/')
+    } 
+  },[token])
+
   return (
     <form onSubmit={onSubmitHandler} className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800'>
       <div className='inline-flex items-center gap-2 mb-2 mt-10'>
